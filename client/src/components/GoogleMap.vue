92e005ba58de797d3aa82198c36952aa8ccc029e
mika018 <miksa108@gmail.com>
2017-08-17 10:30:28 +0100

    Add Vue client

---
+++ client/src/components/GoogleMap.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="google-map" id="gmap"></div>
+</template>
+
+<script>
+export default {
+  name: 'google-map',
+  props: ['name'],
+  data() {
+    return { };
+  },
+  mounted() {
+    const element = document.getElementById('gmap');
+    const options = {
+      zoom: 14,
+      center: new window.google.maps.LatLng(51.501527, -0.1921837),
+    };
+    // eslint-disable-next-line
+    const map = new window.google.maps.Map(element, options);
+  },
+};
+</script> 
+
+<style scoped>
+.google-map {
+  width: 800px;
+  height: 600px;
+  margin: 0 auto;
+  background: gray;
+}
+</style>
